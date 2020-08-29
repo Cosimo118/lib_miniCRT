790d12f912069179e15cb4da497fd1cdfc17b67f
Cosimo <747991257@qq.com>
2020-08-29 14:15:18 +0800

    lib

--- minicrt.h
+++ minicrt.h
@@ -1,3 +1,25 @@
+#ifndef __MINI_CRT_H__
+#define __MINI_CRT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// malloc
+#ifndef NULL
+#define NULL (0)
+#endif
+void free(void* ptr);
+void* malloc(unsigned size);
+static int brk(void* end_data_segment);
+int mini_crt_init_heap();
+
+//string
+char* itoa(int n, char* str, int radix);
+int strcmp (const char* src, const char* dst);
+char* strcpy (char* dest, const char* src);
+unsigned strlen (const char* str);
+
+//file && io
 typedef int FILE;
 #define EOF (-1)
 
@@ -9,4 +31,31 @@ typedef int FILE;
 #define stdin ((FILE*)0)
 #define stdout ((FILE*)1)
 #define stderr ((FILE*)2)
+#endif
+
+int mini_crt_init_io();
+FILE* fopen (const char* filename, const char* mode);
+int fread(void* buffer, int size, int count, FILE* stream);
+int fwrite (const void* buffer, int size, int count, FILE* stream);
+int fclose (FILE* fp);
+int fseek (FILE* fp, int offset, int set);
+
+//printf
+int fputc(int c, FILE* stream);
+int fputs (const char* str, FILE* stream);
+int printf (const char* format, ...);
+int fprintf (FILE* stream, const char* format, ...);
+
+//internal
+void do_global_ctors();
+void mini_crt_call_exit_routine();
+
+//atexit
+typedef void (*atexit_func_t)( void );
+int atexit(atexit_func_t func);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
